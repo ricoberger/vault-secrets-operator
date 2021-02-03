0d80eb125e278b2da3810d160974a26afc705d95
Alexandra Tudor <atudor@adobe.com>
2021-02-03 13:46:15 +0200

    Comment adjustment

--- main.go
+++ main.go
@@ -80,7 +80,7 @@ func main() {
 	// Add support for MultiNamespace set in WATCH_NAMESPACE (e.g ns1,ns2)
 	if strings.Contains(watchNamespace, ",") {
 		setupLog.Info("manager set up with multiple namespaces", "namespaces", watchNamespace)
-		// remove extra whitespaces (e.g. WATCH_NAMESPACE=ns1, ns2)
+		// remove whitespaces (e.g. WATCH_NAMESPACE=ns1, ns2)
 		watchNamespace = strings.ReplaceAll(watchNamespace, " ", "")
 		// configure cluster-scoped with MultiNamespacedCacheBuilder
 		options.Namespace = ""
